1032f27d8859266e84e2f026bb57a8fb9b9f859b
JODACHSE <jonathandavidchavarrosegura@gmail.com>
2025-02-28 17:43:58 -0500

    Added html bootstrap with theme darkly

--- SegundaActividad/app.py
+++ SegundaActividad/app.py
@@ -6,7 +6,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def home():
-    """Retornamos una pagina html con bootstrap"""
-    return render_template('index.html')
+def index():
+    """retornamos la pagina web"""
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
