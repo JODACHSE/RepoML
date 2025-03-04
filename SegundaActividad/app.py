0c780a100c59c8e94ec28787a0206d7229f201fc
nixonbf96 <nixonbf96@gmail.com>
2025-03-04 01:10:59 -0500

    Add new home route and templates for Flask application

--- SegundaActividad/app.py
+++ SegundaActividad/app.py
@@ -6,11 +6,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route("/index")
 @app.route("/")
 def index():
     """retornamos la pagina web"""
     return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("html/home.html")
 
 
 if __name__ == '__main__':
