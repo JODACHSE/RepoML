d0784f09a2122f7b5dc1582f73e3ae9eaed3a180
JODACHSE <jonathandavidchavarrosegura@gmail.com>
2025-03-16 20:25:21 -0500

    Add docstring and update import path in adios_mundo.py

--- python/S2/adios_mundo.py
+++ python/S2/adios_mundo.py
@@ -1,6 +1,9 @@
 # Machine learning - profundizacion II - Nixon Andre Bohorquez
+"""
+Adios usuario en python
+"""
 
-from python.S1.hola_mundo import name
+from python.S2.hola_mundo import name
 
 print('Adios ' + name)
 print('Respuesta del hola mundo')
